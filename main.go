c0129ee6c76199bbcf8e2751ba15d4977ace1bb5
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-24 13:29:11 +1300

    basic server set up
    basic server set up

---
+++ main.go
@@ -0,0 +1,33 @@
+package main 
+
+import (
+	"fmt"
+	socketio "github.com/googollee/go-socket-io"
+)
+
+
+func main() {
+	fmt.Println("Hello World")
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.On("connection", func(so socketio.Socket){
+		log.Println("New Connection")
+
+		so.Join("chat")
+
+		so.On("chat message", func(msg string) {
+			log.Println("Message Received From Client: ", msg)
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+	})
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	http.Handle("/socket.io/", server)
+	log.Fatal(http.ListenAndServe("5000", nil))
+}
